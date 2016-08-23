88d2bf13d6d06079c58d7cba1e5a74244006b465
Nikolay Dynik <dyniknikolay@gmail.com>
2016-08-23 15:44:19 +0300

    new directory "Projects"

--- Web/Modus/js/hover.js
+++
@@ -1,9 +0,0 @@
-$('.four_blocks .one_bl .btn_read').hover(
-    function() {
-        var id = $(this).attr('id');
-        $('#' + id + '_d').addClass('hover');
-    }, function() {
-        var id = $(this).attr('id');
-        $('#' + id + '_d').removeClass('hover');
-    }
-);
